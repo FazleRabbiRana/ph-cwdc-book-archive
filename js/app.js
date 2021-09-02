24b4fae958aab51877287b48a2577789ebe5f5e2
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-09-02 13:53:12 +0600

    completed all functionalities

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
-// notice box
-const noticeText = (
-	isError = true,
-	text = 'Something went wrong. Try again later.'
-) => {
+// loading spinner
+const loadingSpinner = displayStyle => {
+	document.getElementById('loading_spinner').style.display = displayStyle;
+}
+
+// notice text
+const noticeText = (text, colorClass) => {
 	const noticeBox = document.getElementById('notice_box');
-	if (isError === true) {
-		noticeBox.innerHTML = `
-			<p class="d-inline-block bg-danger text-light py-2 px-3">${text}</p>
-		`;
-	} else {
-		noticeBox.innerHTML = `
-			<p class="d-inline-block bg-success text-light py-2 px-3 h5">${text}</p>
-		`;
-	}
-};
+	noticeBox.innerHTML = `
+		<h5 class="${colorClass} mb-5">${text}</h5>
+	`;
+}
 
 // search books
 const searchBooks = async () => {
@@ -21,52 +17,69 @@ const searchBooks = async () => {
 	const searchText = searchInput.value;
 	// clear input field
 	searchInput.value = '';
-
-	if (searchText === '') {
-		alert('Type something');
-		// const noticeBox = document.getElementById('notice_box');
-		// noticeBox.innerHTML = `
-		// 	<h5 class="d-inline-block bg-danger text-light p-2">Oops! Write something before search.</h5>
-		// `;
-		// noticeText(true, 'Oops! Write something before search.');
+	// clear previous notice
+	document.getElementById('notice_box').textContent = '';
+	// clear previous search result
+	document.getElementById('search_result').textContent = '';
+	
+	if(searchText === '') {
+		noticeText('Type something before search.', 'text-danger');
 	} else {
+		loadingSpinner('block');
+
 		const url = `http://openlibrary.org/search.json?q=${searchText}`;
 		try {
 			const res = await fetch(url);
 			const data = await res.json();
-			displayBooks(data.docs);
+			displayBooks(data);
 		} catch (err) {
 			console.log(err);
+			noticeText('Something went wrong. Try again later.', 'text-danger');
+			loadingSpinner('none');
 		}
 	}
-};
+}
 
 // display books
-const displayBooks = books => {
-	console.log(books);
+const displayBooks = data => {
+	console.log(data);
+	const books = data.docs;
+	const booksContainer = document.getElementById('search_result');
 
-	if (books && books.length !== 0) {
-		books.slice(0, 10).forEach(book => {
-			// console.log(book);
-			const booksContainer = document.getElementById('search_result');
+	if(books && books.length > 0) {
+		books.forEach(book => {
+			const noDataText = `<span class="text-danger">not available.</span>`;
 			const singleBook = document.createElement('div');
 			singleBook.classList.add('col');
 			singleBook.innerHTML = `
 				<div class="card h-100 bg-light">
-					<img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="${book.title}">
-					<div class="card-body text-dark">
-						<h4 class="card-title">${book.title}</h4>
-						<cite><b>Author :</b> ${book.author_name[0]}</cite>
+					<div class="cover-image p-3 d-flex align-items-center justify-content-center">
+						${book.cover_i ? `<img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="${book.title}">` : noDataText}
 					</div>
-					<div class="card-footer">
-						<small class="d-block text-muted">First publish year: <span class="text-dark">${book.first_publish_year}</span></small>
-						<small class="d-block text-muted">Publisher: <span class="text-dark">${book.publisher}</span></small>
+					<div class="card-body pt-0 text-dark">
+						<h4 class="card-title">${book.title}</h4>
+						<p class="mb-0">
+							<b>Author :</b> 
+							<cite>${book.author_name ? book.author_name[0] : noDataText}</cite>
+						</p>
+					</div>
+					<div class="card-footer bg-primary bg-opacity-10">
+						<small class="d-block text-muted">First publish year : <span class="text-dark">${book.first_publish_year ? book.first_publish_year : noDataText}</span></small>
+						<small class="d-block text-muted">Publisher : <span class="text-dark">${book.publisher ? book.publisher[0] : noDataText}</span></small>
 					</div>
 				</div>
 			`;
 			booksContainer.appendChild(singleBook);
 		});
+		// show result number text
+		const resultNumberText = `
+			Showing <span class="px-2 bg-success text-light rounded">${books.length}</span> results out of <span class="p-y1 px-2 bg-success text-light rounded">${data.numFound}</span>
+		`;
+		noticeText(resultNumberText, 'text-success');
+		// hide loading spinner
+		loadingSpinner('none');
 	} else {
-		console.log('no match!');
+		noticeText('Oops, no result found!', 'text-warning');
+		loadingSpinner('none');
 	}
-};
+}
